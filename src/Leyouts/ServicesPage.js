ef60ad162ef3e0ef6ab46d9d2106f293d516e5db
Dominik Proficz <dominik.proficz@gmail.com>
2019-10-09 00:05:38 +0200

    Adding structure for services and basic styles

--- src/Leyouts/ServicesPage.js
+++ src/Leyouts/ServicesPage.js
@@ -1,8 +1,131 @@
-import React from 'react';
-import "../css/maine.css"
+import React from "react";
+import "../css/maine.css";
 
 const ServicesPage = () => {
-    return ( <h1 className="ServicesPage">...Usługi...</h1> );
-}
- 
+  return (
+    <div className="services-page-wrapper">
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+      <div className="product-box">
+        <img
+          className="product-box__img"
+          src="http://placehold.jp/150x150.png"
+          alt="image"
+        />
+        <h2 className="product-box__title">Wymiana opon</h2>
+        <p className="product-box__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum
+          autem libero sequi impedit ducimus explicabo eum quo voluptatibus!
+          Deserunt, consequatur laudantium? Minus magni minima ipsa impedit iste
+          sint ab.
+        </p>
+        <button className="product-box__button">Umów wizytę</button>
+      </div>
+    </div>
+  );
+};
+
 export default ServicesPage;
